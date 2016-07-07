e0572f6e555d469cfbb963db4019ebe52176aab7
cmenezes <cmenezes@uncharted.software>
2016-07-07 16:13:05 -0400

    randomdistance calculation if no close pair

--- api/sim/alive.go
+++ api/sim/alive.go
@@ -5,7 +5,6 @@ import (
 	"math/rand"
 
 	"github.com/go-gl/mathgl/mgl32"
-	"github.com/unchartedsoftware/plog"
 )
 
 // RandomPosition returns a random vec3
@@ -19,19 +18,21 @@ func RandomPosition() mgl32.Vec3 {
 
 // AliveAI processes the organism for the given state
 func AliveAI(update *Update, updates map[string]*Update, organism *Organism, perception *PerceptionResults) {
-	update.State.Position = RandomPosition()
+	update.State.Position = determineNewDirection(organism, perception)
 	update.State.Type = "alive"
 }
 
 func determineNewDirection(currentOrganism *Organism, perception *PerceptionResults) mgl32.Vec3 {
 	var closestPair = &DistancePair{Distance: math.Inf(1)}
 	for _, pair := range perception.DistancePairs {
-		if pair.Distance < closestPair.Distance && pair.Distance > 0 && currentOrganism.ID != pair.Organism.ID {
-			log.Info(pair)
+		if pair.Distance < closestPair.Distance && pair.Distance > 0 {
 			closestPair = pair
 		}
 	}
-	log.Info(closestPair.Organism.State.Position)
-	log.Info(currentOrganism.State.Position)
+
+	if nil == closestPair.Organism.State {
+		return RandomPosition()
+	}
+
 	return currentOrganism.State.Position.Sub(closestPair.Organism.State.Position)
 }
